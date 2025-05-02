6655faab0c84f767abaebf4010c12459d7a2bfcc
megpc <leshatelf@gmail.com>
2025-05-02 19:35:52 +0300

    добавил путь к static

--- django_kids_quest/urls.py
+++ django_kids_quest/urls.py
@@ -15,12 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth.views import LogoutView, LoginView
-# from django.urls import handler403
+from django.conf.urls.static import static
+from django.conf import settings
 from . import views
-
-# handler403 = view.permission_denied
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -40,5 +39,4 @@ urlpatterns = [
     path("event_list/", views.event_list, name="event_list"),
     path("empl/", views.empl, name="empl"),
 
-    
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
